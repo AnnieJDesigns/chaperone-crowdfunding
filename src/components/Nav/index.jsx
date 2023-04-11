c0e8a830648ac916266dbf88f7e4ff2ecf10804c
AnnieJDesigns <114784081+AnnieJDesigns@users.noreply.github.com>
2023-04-11 14:06:23 +1000

    corrected nav.css

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import {useNavigate} from 'react-router-dom';
-import './nav.css'
+import './nav.css';
 import StyledLink from '../Link/StyledLink';
 
 function Nav() {
